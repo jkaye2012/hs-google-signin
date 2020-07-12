7aed45070b45d36388205298cbd3833e045665db
Jordan Kaye <jordan.kaye2@gmail.com>
2020-07-12 09:55:35 -0600

    First pass implementation
    Internal module structure created, ready for tests to be written.

--- src/Web/Google/Certificates.hs
+++ src/Web/Google/Certificates.hs
@@ -1,3 +1,56 @@
 module Web.Google.Certificates
   (
+    googleCertificates
+  , PemCerts
+  , Expiry
+  , GoogleCerts(..)
+  , CertificateError(..)
+  , mkGoogleCerts
+  , parseResponse
   ) where
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C
+import qualified Data.Map as Map
+import qualified Data.Text as T
+import qualified Data.Time as Time
+import qualified Network.HTTP.Simple as HTTP
+import qualified Network.HTTP.Types.Header as Header
+import Control.Monad.IO.Class (MonadIO)
+
+type PemCerts = Map.Map T.Text T.Text
+type Expiry = Time.UTCTime
+data GoogleCerts = GoogleCerts PemCerts Expiry
+                   deriving (Show)
+
+data CertificateError = NetworkError Int
+                      | ParsingError HTTP.JSONException
+                      | MissingExpiry
+                      | MalformedExpiry String
+                      deriving (Show)
+
+mkGoogleCerts :: PemCerts -> String -> Either CertificateError GoogleCerts
+mkGoogleCerts pem exp =
+  case parsed of
+    Just t -> Right (GoogleCerts pem t)
+    Nothing -> Left (MalformedExpiry exp)
+  where
+    parsed = Time.parseTimeM True Time.defaultTimeLocale "%a, %d %b %Y %H:%M:%S %Z" exp
+
+parseResponse :: HTTP.Response (Either HTTP.JSONException PemCerts) -> Either CertificateError GoogleCerts
+parseResponse resp =
+    case (HTTP.getResponseStatusCode resp, HTTP.getResponseBody resp, HTTP.getResponseHeader Header.hExpires resp) of
+      (200, Right certs, exp : []) -> mkGoogleCerts certs (C.unpack exp)
+      (200, Left exc, _) -> Left (ParsingError exc)
+      (200, _, _) -> Left MissingExpiry
+      (status, _, _) -> Left (NetworkError status)
+
+rawCertificates :: (MonadIO m) => m (HTTP.Response (Either HTTP.JSONException PemCerts))
+rawCertificates = HTTP.httpJSONEither (HTTP.parseRequest_ "https://www.googleapis.com/oauth2/v1/certs")
+
+googleCertificates :: (MonadIO m) => m (Either CertificateError GoogleCerts)
+googleCertificates = do
+  resp <- rawCertificates
+  return (parseResponse resp)
